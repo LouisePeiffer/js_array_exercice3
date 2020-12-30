a56cc240c5633ab25c3513dfbb85c4f422f1ccb9
Louise <louisepeiffer@hotmail.com>
2020-12-30 15:54:29 +0100

    wsh

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,11 @@ fruits.push('Louiseeeee')
 console.log(fruits);
 fruits.splice(2, 1, "Bounce")
 console.log(fruits);
+
+// Autres méthodes
+fruits[0]='test'
+console.log(fruits);
+
+fruits[fruits.indexOf('fraise')] = 'test2'
+console.log(fruits);
+
